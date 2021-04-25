61c1305aeadd7e2b8fd38099e250b7617f52ac56
boyang9602 <30889920+boyang9602@users.noreply.github.com>
2021-04-25 14:08:04 -0400

    Update gen.py

--- scripts/gen.py
+++ scripts/gen.py
@@ -18,6 +18,3 @@ for pro in metadata:
 		for patch in bug['patches']:
 			os.system(f'defects4j checkout -p {name} -v {bid}b -w {name}/{bid}/{name}{bid}_{patch["ID"]}')
 			os.system(f'sed -i "/d4j.classes.modified/c\\d4j.classes.modified={",".join(patch["modified_classes"])}" {name}/{bid}/{name}{bid}_{patch["ID"]}/defects4j.build.properties')
-			break
-		break
-	break
