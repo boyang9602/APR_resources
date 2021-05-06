3d9173310b23e160957748c6ff508d50cb8e711a
Bo Yang <boyang9602@gmail.com>
2021-05-05 23:20:02 -0400

    update script, fix classpaths, fix multiple modified files, changed modified files location

--- scripts/gen4finvs.sh
+++ scripts/gen4finvs.sh
@@ -5,24 +5,49 @@ IFS=',' read -ra tcarr <<< $raw
 for tc in ${tcarr[@]}; do
         test_cases="$test_cases$tc "
 done
-path="$DAIKONDIR/daikon.jar:lib/*:build/classes/:build/test/:build/lib/rhino.jar:build/lib/rhino1_7R5pre/js.jar:/home/ubuntu/launcher/"
-pptpattern=$(cat defects4j.build.properties | grep d4j.classes.modified | sed 's/d4j.classes.modified=\(.*\)/\1/' | sed 's/\.[A-Za-z]*$/\./')
-pptpattern="--ppt-select-pattern=^${pptpattern//\./\\\.}"
-defects4j compile
-if [ -d ft_invariants_files ]; then
-	rm -rf ft_invariants_files
+CLASSPATH=$(find "$PWD" -name '*.jar' -type f -printf ':%p\n' | sort -u | tr -d '\n')
+project=$(tail -n2 defects4j.build.properties | head -n1 | sed 's/d4j\.project\.id=\(.*\)/\1/')
+if [[ $project = Chart ]]; then
+	path="$DAIKONDIR/daikon.jar:build/:build-tests/$CLASSPATH:/home/ubuntu/launcher/"
+elif [[ $project = Math ]]; then
+	path="$DAIKONDIR/daikon.jar:target/classes/:target/test-classes/$CLASSPATH:/home/ubuntu/launcher/"
+elif [[ $project = Time ]]; then
+	path="$DAIKONDIR/daikon.jar:target/classes/:target/test-classes/$CLASSPATH:/home/ubuntu/launcher/"
+elif [[ $project = Lang ]]; then
+	path="$DAIKONDIR/daikon.jar:target/classes/:target/test-classes/:target/tests/$CLASSPATH:/home/ubuntu/launcher/"
+elif [[ $project = Mockito ]]; then
+	path="$DAIKONDIR/daikon.jar:target/classes/:target/test-classes/$CLASSPATH:/home/ubuntu/launcher/"
+elif [[ $project = Lang ]]; then
+	path="$DAIKONDIR/daikon.jar:build/classes/:build/test/$CLASSPATH:/home/ubuntu/launcher/"
 fi
-mkdir ft_invariants_files
-cmd="java -Xmx43G -cp $path daikon.DynComp '$pptpattern' --output-dir=ft_invariants_files TestRunner $test_cases |& tee ft_invariants_files/dyncomp-output.txt"
-echo $cmd
-echo $cmd >> ft_invariants_files/cmds.txt
-eval $cmd
-cmd="java -Xmx43G -cp $path daikon.Chicory '$pptpattern' --heap-size=43G --comparability-file=ft_invariants_files/TestRunner.decls-DynComp --output-dir=ft_invariants_files TestRunner $test_cases |& tee ft_invariants_files/chicory-output.txt"
-echo $cmd
-echo $cmd >> ft_invariants_files/cmds.txt
-eval $cmd
-cmd="java -Xmx43G -cp $DAIKONDIR/daikon.jar daikon.Daikon -o ft_invariants_files/closure.inv.gz --no_text_output ft_invariants_files/TestRunner.dtrace.gz |& tee ft_invariants_files/daikon-output.txt"
-echo $cmd
-echo $cmd >> ft_invariants_files/cmds.txt
-eval $cmd
-echo $? > ft_invariants_files/result.txt
+
+defects4j compile
+
+a=0
+while read classes ; do
+	IFS=',' read -ra classArr <<< "$classes"
+	pptpattern=""
+	for class in "${classArr[@]}"; do
+		pptpattern="--ppt-select-pattern=^${class//\./\\\.} $pptpattern"
+	done
+
+	folder=ft_invariants_files$a
+	if [[ -d $folder ]]; then
+		rm -rf $folder
+	fi
+	mkdir $folder
+	cmd="java -Xmx43G -cp $path daikon.DynComp '$pptpattern' --output-dir=$folder TestRunner $test_cases |& tee $folder/dyncomp-output.txt"
+	echo $cmd
+	echo $cmd >> $folder/cmds.txt
+	eval $cmd
+	cmd="java -Xmx43G -cp $path daikon.Chicory '$pptpattern' --heap-size=43G --comparability-file=$folder/TestRunner.decls-DynComp --output-dir=$folder TestRunner $test_cases |& tee $folder/chicory-output.txt"
+	echo $cmd
+	echo $cmd >> $folder/cmds.txt
+	eval $cmd
+	cmd="java -Xmx43G -cp $DAIKONDIR/daikon.jar daikon.Daikon -o $folder/closure.inv.gz --no_text_output $folder/TestRunner.dtrace.gz |& tee $folder/daikon-output.txt"
+	echo $cmd
+	echo $cmd >> $folder/cmds.txt
+	eval $cmd
+	echo $? > $folder/result.txt
+	a=$((a+1))
+done <modified_classes
